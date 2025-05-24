360ed3b1c82043bb613cf35363c8d09dc2e9165b
2darshan1212 <darshamisaal1212@gmail.com>
2025-05-24 13:43:17 +0530

    updated with new codeejh

--- frontend/src/components/chat/ChatLayout.jsx
+++ frontend/src/components/chat/ChatLayout.jsx
@@ -6,28 +6,33 @@ import ChatRightSideBar from "./ChatRightSideBar";
 
 const ChatLayout = () => {
   return (
-    <div>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        {/* Main Layout */}
-        <div className="flex flex-1 flex-col md:flex-row gap-4 p-4">
-          {/* Left Sidebar sticky on md+ */}
-          <aside className="hidden md:flex md:flex-col md:w-20 lg:w-64">
+    <div className="h-screen overflow-hidden">
+      <div className="h-full flex flex-col">
+        {/* Header - always visible, adaptive height */}
+        <div className="flex-shrink-0">
+          <Header />
+        </div>
+        
+        {/* Main Layout - takes remaining height */}
+        <div className="flex flex-1 flex-col md:flex-row overflow-hidden">
+          {/* Left Sidebar - hidden on mobile, visible on tablet+ */}
+          <aside className="hidden md:flex md:flex-col md:w-16 lg:w-64 border-r border-gray-200 overflow-y-auto">
             <Leftsidebar />
           </aside>
 
-          {/* Center Content - Give it more space since conversation list is now in a drawer */}
-          <div className="flex-1 md:max-w-[calc(100%-320px)] lg:max-w-[calc(100%-600px)]">
+          {/* Center Content - scrollable, takes available height */}
+          <div className="flex-1 overflow-hidden flex flex-col h-full md:max-w-[calc(100%-16rem)] lg:max-w-[calc(100%-30rem)]">
             <Outlet />
           </div>
 
-          {/* Right Sidebar */}
-          <aside className="hidden lg:block lg:w-[300px] mr-5">
+          {/* Right Sidebar - hidden on small screens, visible on large screens */}
+          <aside className="hidden lg:flex lg:flex-col lg:w-64 xl:w-80 border-l border-gray-200 overflow-y-auto">
             <ChatRightSideBar/>
           </aside>
         </div>
-        {/* Left Sidebar fixed at bottom on mobile */}
-        <div className="fixed bottom-0 left-0 right-0 block md:hidden p-2 shadow-t bg-white border-t">
+        
+        {/* Mobile Navigation - fixed at bottom */}
+        <div className="fixed bottom-0 left-0 right-0 block md:hidden py-1 px-2 shadow-lg bg-white border-t z-40">
           <Leftsidebar />
         </div>
       </div>
